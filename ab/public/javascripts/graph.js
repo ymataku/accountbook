425737884ebe72cc521b339e0cabed8376f56d95
duck <takuya.yt@outlook.jp>
2021-05-06 19:29:19 +0900

    make it better

---
+++ ab/public/javascripts/graph.js
@@ -0,0 +1,11 @@
+var app3 = new Vue({
+    el:'#app3',
+    
+    data:{
+        message:'整数を入力して',
+        message2:'整数を入力して',
+        message3:'整数を入力して',
+
+    }
+})
+
---
+++ ab/public/javascripts/login.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el:'#app',
+    watch:{
+        message:function(long){
+            this.error.require = (long.length < 1) ? true : false; 
+        }
+    },
+    data:{
+        message:'',
+        error:{
+            require:false
+
+        }
+
+    }
+})
+
+var app2 = new Vue({
+    el:'#app2',
+    watch:{
+        message:function(e,e2){
+            this.error.require = (e.length < 1) ? true:false;
+            
+        }
+    },
+    data:{
+        message:'',
+        error:{
+            require:false
+        }
+    }
+})
